79c98bc86b4e867d01a1a5ca1752b4914600731b
wiltonrsp <49824577+wiltonrsp@users.noreply.github.com>
2019-06-28 11:26:05 -0300

    att

---
+++ sidebar.php
@@ -0,0 +1,63 @@
+    <aside class="sidebar-left">
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+                    <div class="circle">
+                    <img class="img" src="/../novo/imagem/logo.png">
+            </div>
+            <style></style>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="sidebar-menu">
+              
+              <li class="treeview">
+                <a href="index.php">
+                 <span>Inicio</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+              </li>
+              <li class="treeview">
+                <a href="#" id="relatorio">
+                <i class="relatorio"></i> <span>Relatórios</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                
+                <ul class="treeview-menu">
+                  <li><a href="relatorio.php?papel=5"><i class="fa fa-circle-o"></i>Total</a></li>
+                  <li><a href="relatorio.php?papel=1"><i class="fa fa-circle-o"></i>Analista</a></li>
+                  <li><a href="relatorio.php?papel=2"><i class="fa fa-circle-o"></i>Arquiteto</a></li>
+                  <li><a href="relatorio.php?papel=3"><i class="fa fa-circle-o"></i>Desenvolvedor</a></li>
+                  <li><a href="relatorio.php?papel=4"><i class="fa fa-circle-o"></i>Testador</a></li>
+                </ul>
+              </a>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class=""></i> <span>Equipe</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <a href="#" id="relatorio">
+                    <li id="add" ><a href="#" data-toggle="modal" data-target="#basicModal2"><i class="fa fa-circle-o"></i>Adicionar Membro</a></li>
+                    <li id="altmembro" ><a href="#" data-toggle="modal" data-target="#basicModal33"><i class="fa fa-circle-o"></i>Alterar Papeis</a></li>
+                    <li id="criacao"><a href="#" data-toggle="modal" data-target="#basicModal99"><i class="fa fa-circle-o"></i>Criar Equipe</a></li>
+                  
+
+                </ul>
+              </li>
+              <li>
+                <a href="login/logout.php">
+                <i class="fa fa-angle-left pull-right"></i> <span>Sair</span>
+               
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>   
+      </nav>
+    </aside>
