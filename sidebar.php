fb3a413888918e7c9d96cfc16a562c12bcfa5e5e
wiltonrsp <49824577+wiltonrsp@users.noreply.github.com>
2019-06-28 09:57:56 -0300

    primary

---
+++ sidebar.php
@@ -0,0 +1,86 @@
+ <link href='SidebarNav.min.css' media='all' rel='stylesheet' type='text/css'/>
+
+    <script src="jquery-1.12.4.min.js"></script>
+    <script src="bootstrap3.3.js"></script>
+    <script src='SidebarNav.min.js' type='text/javascript'></script>
+ <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="netdna.css">
+    <aside class="sidebar-left">
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+                    <div class="circle">
+                    <img class="img" src="sindicato.jfif">
+            </div>
+            <style></style>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="sidebar-menu">
+              
+              <li class="treeview">
+                <a href="index.php">
+                 <span>Inicio</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+              </li>
+              <li class="treeview">
+                <a href="#" id="relatorio">
+                <i class="relatorio"></i> <span>Relatórios</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                
+                <ul class="treeview-menu">
+                  <li><a href="relatorio.php?papel=5"><i class="fa fa-circle-o"></i>Total</a></li>
+                  <li><a href="relatorio.php?papel=1"><i class="fa fa-circle-o"></i>Analista</a></li>
+                  <li><a href="relatorio.php?papel=2"><i class="fa fa-circle-o"></i>Arquiteto</a></li>
+                  <li><a href="relatorio.php?papel=3"><i class="fa fa-circle-o"></i>Desenvolvedor</a></li>
+                  <li><a href="relatorio.php?papel=4"><i class="fa fa-circle-o"></i>Testador</a></li>
+                </ul>
+              </a>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                <i class=""></i> <span>Equipe</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <a href="#" id="relatorio">
+                    <li id="add" ><a href="#" data-toggle="modal" data-target="#basicModal2"><i class="fa fa-circle-o"></i>Adicionar Membro</a></li>
+                    <li id="altmembro" ><a href="#" data-toggle="modal" data-target="#basicModal33"><i class="fa fa-circle-o"></i>Alterar Papeis</a></li>
+                    <li id="criacao"><a href="#" data-toggle="modal" data-target="#basicModal99"><i class="fa fa-circle-o"></i>Criar Equipe</a></li>
+                  
+
+                </ul>
+              </li>
+              <li>
+                <a href="login/logout.php">
+                <i class="fa fa-angle-left pull-right"></i> <span>Sair</span>
+               
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>   
+      </nav>
+    </aside>
+     <script>
+      $('.sidebar-menu').SidebarNav()
+    </script>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
