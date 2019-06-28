32d3a8e2d187c51868153c7f67c35b99058f2ff7
wiltonrsp <49824577+wiltonrsp@users.noreply.github.com>
2019-06-28 11:34:38 -0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,10 @@
+ <link href='SidebarNav.min.css' media='all' rel='stylesheet' type='text/css'/>
+
+    <script src="jquery-1.12.4.min.js"></script>
+    <script src="bootstrap3.3.js"></script>
+    <script src='SidebarNav.min.js' type='text/javascript'></script>
+ <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="netdna.css">
     <aside class="sidebar-left">
       <nav class="navbar navbar-inverse">
         <div class="container-fluid">
@@ -9,7 +16,7 @@
             <span class="icon-bar"></span>
             </button>
                     <div class="circle">
-                    <img class="img" src="/../novo/imagem/logo.png">
+                    <img class="img" src="sindicato.jfif">
             </div>
             <style></style>
           </div>
@@ -17,27 +24,71 @@
             <ul class="sidebar-menu">
               
               <li class="treeview">
-                <a href="index.php">
+                <a href="sidebar.html">
+                  <i class="fa fa-home"></i> 
                  <span>Inicio</span> <i class="fa fa-angle-left pull-right"></i>
                 </a>
               </li>
               <li class="treeview">
                 <a href="#" id="relatorio">
-                <i class="relatorio"></i> <span>Relatórios</span>
+                <i class="fa fa-industry"></i> <span>Cadastrar Empresa</span>
                 <i class="fa fa-angle-left pull-right"></i>
                 
                 <ul class="treeview-menu">
-                  <li><a href="relatorio.php?papel=5"><i class="fa fa-circle-o"></i>Total</a></li>
-                  <li><a href="relatorio.php?papel=1"><i class="fa fa-circle-o"></i>Analista</a></li>
-                  <li><a href="relatorio.php?papel=2"><i class="fa fa-circle-o"></i>Arquiteto</a></li>
-                  <li><a href="relatorio.php?papel=3"><i class="fa fa-circle-o"></i>Desenvolvedor</a></li>
-                  <li><a href="relatorio.php?papel=4"><i class="fa fa-circle-o"></i>Testador</a></li>
+                  <li><a href="relatorio.php?papel=5"><i class="fa fa-circle-o"></i>item1</a></li>
+                  <li><a href="relatorio.php?papel=1"><i class="fa fa-circle-o"></i>item2</a></li>
+                  <li><a href="relatorio.php?papel=2"><i class="fa fa-circle-o"></i>item3</a></li>
+                  <li><a href="relatorio.php?papel=3"><i class="fa fa-circle-o"></i>item4</a></li>
+                  <li><a href="relatorio.php?papel=4"><i class="fa fa-circle-o"></i>item5</a></li>
                 </ul>
               </a>
               </li>
               <li class="treeview">
                 <a href="#">
-                <i class=""></i> <span>Equipe</span>
+                <i class="fa fa-address-card-o"></i> <span>Cadastrar sócio</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <a href="#" id="relatorio">
+                    <li id="add" ><a href="#" data-toggle="modal" data-target="#basicModal2"><i class="fa fa-circle-o"></i>Adicionar Membro</a></li>
+                    <li id="altmembro" ><a href="#" data-toggle="modal" data-target="#basicModal33"><i class="fa fa-circle-o"></i>Alterar Papeis</a></li>
+                    <li id="criacao"><a href="#" data-toggle="modal" data-target="#basicModal99"><i class="fa fa-circle-o"></i>Criar Equipe</a></li>
+                  
+
+                </ul>
+              </li>
+                <li class="treeview">
+                <a href="#">
+                <i class="fa fa-calendar"></i> <span>Agenda</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <a href="#" id="relatorio">
+                    <li id="add" ><a href="#" data-toggle="modal" data-target="#basicModal2"><i class="fa fa-circle-o"></i>Adicionar Membro</a></li>
+                    <li id="altmembro" ><a href="#" data-toggle="modal" data-target="#basicModal33"><i class="fa fa-circle-o"></i>Alterar Papeis</a></li>
+                    <li id="criacao"><a href="#" data-toggle="modal" data-target="#basicModal99"><i class="fa fa-circle-o"></i>Criar Equipe</a></li>
+                  
+
+                </ul>
+              </li>
+                  <li class="treeview">
+                <a href="#">
+
+                <i class="fa fa-book"></i> <span>Relatórios</span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <a href="#" id="relatorio">
+                    <li id="add" ><a href="#" data-toggle="modal" data-target="#basicModal2"><i class="fa fa-circle-o"></i>Adicionar Membro</a></li>
+                    <li id="altmembro" ><a href="#" data-toggle="modal" data-target="#basicModal33"><i class="fa fa-circle-o"></i>Alterar Papeis</a></li>
+                    <li id="criacao"><a href="#" data-toggle="modal" data-target="#basicModal99"><i class="fa fa-circle-o"></i>Criar Equipe</a></li>
+                  
+
+                </ul>
+              </li>
+                   <li class="treeview">
+                <a href="#">
+                <i class="fa fa-users"></i> <span>Criar contas</span>
                 <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
@@ -51,6 +102,7 @@
               </li>
               <li>
                 <a href="login/logout.php">
+                  <i class="fa fa-mail-forward"> </i>
                 <i class="fa fa-angle-left pull-right"></i> <span>Sair</span>
                
                 </a>
@@ -61,3 +113,19 @@
         </div>   
       </nav>
     </aside>
+     <script>
+      $('.sidebar-menu').SidebarNav()
+    </script>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
