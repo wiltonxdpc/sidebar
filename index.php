695967a4f5cdd097c22763a41222a88b7f6d349d
Travis <travis@travis-ci.org>
2019-08-24 14:17:45 -0300

    a

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-	$to = 'wiltonricardo6@gmail.com';
-	$subject = 'somente um teste';
-	$message = '<h1>OLA</h1><p>VAI TOMA NO CU</p>';
-	$headers = "from:sender name<wiltonricardo2@outlook.com>\r\n";
-	$headers.="Content-type:text/html\r\n";
-	mail($to,$subject,$message,$headers);
-	
+	echo "chupa cu";
 ?>
 
 
