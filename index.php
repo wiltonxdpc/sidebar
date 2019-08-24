b998f711c2f826aa3f39ad16c0e2cf887d271cbc
Travis <travis@travis-ci.org>
2019-08-24 14:11:28 -0300

    attt

--- index.php
+++ index.php
@@ -1,53 +1,10 @@
 <?php
-	require 'PHPMailer/PHPMailerAutoload.php';
-	
-	$Mailer = new PHPMailer();
-	
-	//Define que será usado SMTP
-	$Mailer->IsSMTP();
-	
-	//Enviar e-mail em HTML
-	$Mailer->isHTML(true);
-	
-	//Aceitar carasteres especiais
-	$Mailer->Charset = 'UTF-8';
-	
-	//Configurações
-	$Mailer->SMTPAuth = true;
-	$Mailer->SMTPSecure = 'ssl';
-	
-	//nome do servidor
-	$Mailer->Host = 'smtp.gmail.com';
-	//Porta de saida de e-mail 
-	$Mailer->Port = 587;
-	
-	//Dados do e-mail de saida - autenticação
-	$Mailer->Username = 'wiltonricardo6@gmail.com';
-	$Mailer->Password = '@wiltonxdK1';
-	
-	//E-mail remetente (deve ser o mesmo de quem fez a autenticação)
-	$Mailer->From = 'wiltonricardo6@gmail.com';
-	
-	//Nome do Remetente
-	$Mailer->FromName = 'Celke';
-	
-	//Assunto da mensagem
-	$Mailer->Subject = 'Titulo - Recuperar Senha';
-	
-	//Corpo da Mensagem
-	$Mailer->Body = 'Conteudo do E-mail';
-	
-	//Corpo da mensagem em texto
-	$Mailer->AltBody = 'conteudo do E-mail em texto';
-	
-	//Destinatario 
-	$Mailer->AddAddress('wiltonricardo2@outlook.com');
-	
-	if($Mailer->Send()){
-		echo "E-mail enviado com sucesso";
-	}else{
-		echo "Erro no envio do e-mail: " . $Mailer->ErrorInfo;
-	}
+	$to = 'wiltonricardo6@gmail.com';
+	$subject = 'somente um teste';
+	$message = '<h1>OLA</h1><p>VAI TOMA NO CU</p>';
+	$headers = "from:sender name<wiltonricardo2@outlook.com>\r\n";
+	$headers.="Content-type:text/html\r\n";
+	mail($to,$subject,$message,$headers);
 	
 ?>
 
