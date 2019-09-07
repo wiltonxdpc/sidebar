cdc1854e3c593766b80f35ccb087b5c9edf3be17
wiltonxdpc <50665688+wiltonxdpc@users.noreply.github.com>
2019-09-07 11:46:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $connection = new PDO($dsn, $username, $password);
    {
      echo "Database connection success.";
    }
-if(pg_select_db($con,"d8g5vkl3hk84ai")){
+if(pg_select_db($connection,"d8g5vkl3hk84ai")){
 	echo "certo";
 }	
 $query = "INSERT INTO Users (nome) VALUES ('wilton')"; 
