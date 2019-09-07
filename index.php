c6d47fc57faf096c6a3ad8784a1dbbea59b36a1d
wiltonxdpc <50665688+wiltonxdpc@users.noreply.github.com>
2019-09-07 11:23:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $connection = new PDO($dsn, $username, $password);
 } catch(PDOException $e) {
 
 }
-if($this->con=pg_connect("host=ec2-23-23-92-204.compute-1.amazonaws.com","movgpzvubxrkru","f4117a6654430998b88fafb8fff17397b647ae20fd38b526e3df636f4edf3d54")){
+if(pg_connect("host=ec2-23-23-92-204.compute-1.amazonaws.com","movgpzvubxrkru","f4117a6654430998b88fafb8fff17397b647ae20fd38b526e3df636f4edf3d54")){
 			echo "besta";
 		}else{
 			echo "otario";
