880d46138cd716a7b98911f25d1d4b7acf667194
wiltonxdpc <50665688+wiltonxdpc@users.noreply.github.com>
2019-09-07 11:10:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-	echo "chupa cu";
-?>
+$dsn = 'host=ec2-23-23-92-204.compute-1.amazonaws.com
+;dbname=d8g5vkl3hk84ai';
+$username = 'movgpzvubxrkru';
+$password = 'f4117a6654430998b88fafb8fff17397b647ae20fd38b526e3df636f4edf3d54';
+try {
+$connection = new PDO($dsn, $username, $password);
+	echo"cacho";
+} catch(PDOException $e) {
 
-
-
+}
